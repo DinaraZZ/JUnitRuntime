1b111c75fa0ff97c157ccbbf0efe6f5399abf66c
DinaraZZ <dinara.rakhymbekova@gmail.com>
2024-09-18 01:03:32 +0500

    bank account tests 2

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,3 +1,6 @@
+import exceptions.InsufficientFundsException;
+import exceptions.NegativeAmountException;
+
 public class BankAccount {
 
     private boolean isBlocked = false;
@@ -17,7 +20,7 @@ public class BankAccount {
     }
 
     public void activate(String currency) {
-        this.amount = 0;
+        this.amount = (Integer) 0;
         this.currency = currency;
     }
 
@@ -26,6 +29,33 @@ public class BankAccount {
             throw new IllegalStateException("Счёт не активирован.");
         }
         return this.amount;
+    }
+
+    public void deposit(int amount) {
+        if (isBlocked) throw new IllegalStateException("Счёт заблокирован.");
+        if (currency == null) throw new IllegalStateException("Счёт не активирован.");
+        if (amount <= 0) throw new NegativeAmountException("Невозможно пополнить счет отрицательным значением.");
+        this.amount += amount;
+    }
+
+    public void withdraw(int amount) {
+        if (isBlocked) throw new IllegalStateException("Счёт заблокирован.");
+        if (currency == null) throw new IllegalStateException("Счёт не активирован.");
+        if (amount <= 0) throw new NegativeAmountException("Невозможно снять отрицательное значение.");
+        if (this.amount < amount) throw new InsufficientFundsException();
+
+        this.amount -= amount;
+    }
+
+    public void transfer(BankAccount otherAccount, int amount) {
+        if (isBlocked || otherAccount.isBlocked) throw new IllegalStateException("Один из счетов заблокирован.");
+        if (currency == null || otherAccount.getCurrency() == null)
+            throw new IllegalStateException("Один из счетов не активирован.");
+        if (amount <= 0) throw new NegativeAmountException("Невозможно снять отрицательное значение.");
+        if (this.amount < amount) throw new InsufficientFundsException();
+
+        this.amount -= amount;
+        otherAccount.deposit(amount);
     }
 
     public String getCurrency() {
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,3 +1,4 @@
+import exceptions.InsufficientFundsException;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Executable;
@@ -45,5 +46,150 @@ public class BankAccountTest {
         assertNull(account.getCurrency());
         IllegalStateException exception = assertThrows(IllegalStateException.class, account::getAmount);
         assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotWithdrawIfNotActivated() {
+        // не должен снимать деньги, если аккаунт не активирован.
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        String expectedMsg = "Счёт не активирован.";
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> account.withdraw(10));
+
+        assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    public void shouldWithdrawIfActivated() {
+        // должен снимать деньги, если аккаунт активирован
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("KZT");
+        account.deposit(20);
+        int expectedAmount = 10;
+        int withdrawAmount = 10;
+
+        account.withdraw(withdrawAmount);
+
+        assertEquals(expectedAmount, account.getAmount());
+    }
+
+    @Test
+    public void shouldNotWithdrawIfAmountIsInsufficient() {
+        // не должен снимать деньги, если не хватает денег на счёте
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("KZT");
+        account.deposit(20);
+        int withdrawAmount = 40;
+
+        assertThrows(InsufficientFundsException.class, () -> account.withdraw(withdrawAmount));
+    }
+
+    @Test
+    public void shouldNotWithdrawIfBlocked() {
+        // не должен снимать деньги, если аккаунт заблокирован
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("KZT");
+        account.block();
+        String expectedMsg = "Счёт заблокирован.";
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> account.withdraw(10));
+
+        assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotTransferIfAccountNotActivated() {
+        // не должен переводить деньги, если аккаунт отправителя не активирован
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        BankAccount otherAccount = new BankAccount("c", "d");
+        String expectedMsg = "Один из счетов не активирован.";
+        otherAccount.activate("KZT");
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> account.transfer(otherAccount, 10));
+
+        assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotTransferIfOtherAccountNotActivated() {
+        // не должен переводить деньги, если аккаунт получателя не активирован
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        BankAccount otherAccount = new BankAccount("c", "d");
+        String expectedMsg = "Один из счетов не активирован.";
+        account.activate("KZT");
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> account.transfer(otherAccount, 10));
+
+        assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotTransferIfAccountBlocked() {
+        // не должен переводить деньги, если аккаунт отправителя заблокирован
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        BankAccount otherAccount = new BankAccount("c", "d");
+        String expectedMsg = "Один из счетов заблокирован.";
+        account.block();
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> account.transfer(otherAccount, 10));
+
+        assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotTransferIfOtherAccountBlocked() {
+        // не должен переводить деньги, если аккаунт получателя заблокирован
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        BankAccount otherAccount = new BankAccount("c", "d");
+        String expectedMsg = "Один из счетов заблокирован.";
+        otherAccount.block();
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> account.transfer(otherAccount, 10));
+
+        assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotTransferIfAmountIsInsufficient() {
+        // не должен переводить деньги, если не счёте недостаточно средств
+        // выбрасывается исключение
+        BankAccount account = new BankAccount("a", "b");
+        BankAccount otherAccount = new BankAccount("c", "d");
+
+        account.activate("KZT");
+        otherAccount.activate("KZT");
+
+        assertThrows(InsufficientFundsException.class,
+                () -> account.transfer(otherAccount, 10));
+    }
+
+    @Test
+    public void shouldTransferIfAmountIsSufficient() {
+        // должен переводить деньги, если не счёте достаточно средств
+        BankAccount account = new BankAccount("a", "b");
+        BankAccount otherAccount = new BankAccount("c", "d");
+        int amount = 20;
+        int transferAmount = 10;
+        int expectedAmount = 10;
+
+        account.activate("KZT");
+        otherAccount.activate("KZT");
+        account.deposit(amount);
+        account.transfer(otherAccount, transferAmount);
+
+        assertEquals(expectedAmount, account.getAmount());
     }
 }
